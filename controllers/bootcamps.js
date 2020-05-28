97b76a83018c019d0a85c03fabd72f0d9cbae0ad
Muhammad Adnan Riaz <andanriaz511@gmail.com>
2020-05-28 19:51:19 +0500

    end of section 4

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,41 +1,71 @@
+const Bootcamp = require('../models/Bootcamp')
+
 //@desc get all bootcamps
 //@route GET /api/v1/bootcamps
 //@access public
 
-exports.getBootcamps = (req,res,next) => { 
-    res
-    .status(200)
-    .json({success:true,msg:'Get All Bootcamps'})
+exports.getBootcamps = async (req,res,next) => { 
+    try {
+        const bootcamps = await Bootcamp.find();
+        res.status(200).json({success:true,count:bootcamps.length,data:bootcamps})
+    } catch (error) {
+     res.status(400).json({success:false})
+    }
 }
 
 //@desc get bootcamp by id 
 //@route GET /api/v1/bootcamps/:id
 //@access public
 
-exports.getBootcamp = (req,res,next) => { 
-    res
-    .status(200)
-    .json({success:true,msg:`Get Bootcamp with id ${req.params.id}`})
+exports.getBootcamp = async (req,res,next) => { 
+   try {
+       const bootcamps = await Bootcamp.findById(req.params.id);
+       if(!bootcamps) { 
+           return res.status(400).json({success:false})
+       }
+       res.status(200).json({success:true,data:bootcamps})
+   } catch (error) {
+    res.status(400).json({success:false})
+   }
 }
 
 //@desc create new bootcamp
 //@route POST /api/v1/bootcamps
 //@access private
 
-exports.createBootcamp = (req,res,next) => { 
-    res
-    .status(201)
-    .json({success:true,msg:`bootcamp created`})
+exports.createBootcamp = async (req,res,next) => { 
+    try {
+        const bootcamp = await Bootcamp.create(req.body)
+        res.status(201).json({
+            success:true,
+            result:bootcamp
+        })
+    } catch (error) {
+        res.status(400).json({success:false})
+    }
 }
 
 //@desc update single bootcamp
 //@route GET /api/v1/bootcamps/:id
 //@access private
 
-exports.updateBootcamp = (req,res,next) => { 
-    res
-    .status(200)
-    .json({success:true,msg:`Bootcamp updated with id ${req.params.id}`})
+exports.updateBootcamp = async (req,res,next) => { 
+    try {
+        const bootcamp = await Bootcamp.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            ({
+                new:true,
+                runValidators:true
+            })
+            )
+        if(!bootcamp) { 
+            return res.status(400).json({success:false})
+        }
+        res.status(200).json({success:true,data:bootcamp})
+    } catch (error) {
+     res.status(400).json({success:false})
+    }  
 }
 
 
@@ -43,8 +73,16 @@ exports.updateBootcamp = (req,res,next) => {
 //@route GET /api/v1/bootcamps/:id
 //@access private
 
-exports.deleteBootcamp = (req,res,next) => { 
-    res
-    .status(200)
-    .json({success:true,msg:`Bootcamp deleted with id ${req.params.id}`})
+exports.deleteBootcamp = async (req,res,next) => { 
+    try {
+        const bootcamp = await Bootcamp.findByIdAndDelete(
+            req.params.id
+            )
+        if(!bootcamp) { 
+            return res.status(400).json({success:false})
+        }
+        res.status(200).json({success:true,data:{}})
+    } catch (error) {
+     res.status(400).json({success:false})
+    } 
 }
--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
-
-//routers
-const bootcamps = require('./routes/bootcamps')
+const colors = require('colors')
+const connectDB = require('./config/db')
 
 //load config file 
 dotenv.config({path:'./config/config.env'})
 
+connectDB()
+
+//Route Files 
+const bootcamps = require('./routes/bootcamps')
+
+
 const app = express()
+
+//Body Parser
+
+app.use(express.json())
 
 //logger
 if(process.env.NODE_ENV === 'development') {
@@ -18,5 +27,11 @@ app.use(morgan('combined'));
 app.use('/api/v1/bootcamps',bootcamps)
 
 const PORT = process.env.PORT || 5000
-app.listen(PORT,
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+const server = app.listen(PORT,
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
+
+process.on('unhandledRejection',(err,promise) => {
+    console.log(`Error: ${err.message}`.red)
+    //Close server & exit process
+    server.close(() => process.exit(1))
+})
