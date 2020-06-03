cb8ebc7dc01b3749db8acd2f46689afffe32ecd9
adnan riaz <andanriaz511@gmail.com>
2020-06-03 17:54:06 +0500

    pushed before mac

---
+++ routes/courses.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+const {
+    getCourses
+} = require('../controllers/courses')
+
+router.get('/').get(getCourses)
+
+module.exports = router
--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const connectDB = require('./config/db')
 const errorMiddleware = require('./middlewares/error')
 
 //load config file 
-dotenv.config({path:'./config/config.env'})
+dotenv.config({ path: './config/config.env' })
 
 connectDB()
 
 //Route Files 
 const bootcamps = require('./routes/bootcamps')
-
+const courses = require('./routes/courses')
 
 const app = express()
 
@@ -21,19 +21,20 @@ const app = express()
 app.use(express.json())
 
 //logger
-if(process.env.NODE_ENV === 'development') {
-app.use(morgan('combined'));
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('combined'));
 }
 
-app.use('/api/v1/bootcamps',bootcamps)
+app.use('/api/v1/bootcamps', bootcamps)
+app.use('/api/v1/courses', courses)
 
-app.use(errorMiddleware )
+app.use(errorMiddleware)
 
 const PORT = process.env.PORT || 5000
 const server = app.listen(PORT,
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
 
-process.on('unhandledRejection',(err,promise) => {
+process.on('unhandledRejection', (err, promise) => {
     console.log(`Error: ${err.message}`.red)
     //Close server & exit process
     server.close(() => process.exit(1))
